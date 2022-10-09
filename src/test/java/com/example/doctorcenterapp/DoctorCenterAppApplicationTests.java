4c90d67875182bfef00acb1c928656b3fdbb7b75
nzharkynbek <nzharkynbek@one.kz>
2022-10-09 22:52:10 +0600

    Initial commit

---
+++ src/test/java/com/example/doctorcenterapp/DoctorCenterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doctorcenterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorCenterAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
