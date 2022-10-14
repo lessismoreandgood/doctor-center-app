77f628bcc7b53f34a05220fd442203a4f0608d73
nzharkynbek <nzharkynbek@one.kz>
2022-10-14 23:15:03 +0600

    feature(01). Added new entities and jpa dependency.

---
+++ src/main/java/com/example/doctorcenterapp/models/Appointment.java
@@ -0,0 +1,37 @@
+package com.example.doctorcenterapp.models;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Table(name = "appointment")
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Appointment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String appointmentId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "regtime")
+    @Transient
+    private String dateOfCreation;
+
+    @Column(name = "date")
+    private String dateOfRegistration;
+
+    @Column(name = "time")
+    private String time;
+
+    @Column(name = "prim")
+    private String prim;
+}
---
+++ src/main/java/com/example/doctorcenterapp/models/User.java
@@ -0,0 +1,40 @@
+package com.example.doctorcenterapp.models;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Table(name = "user")
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String userId;
+
+    @Column(name = "username", nullable = false, unique = true)
+    @NonNull()
+    private String email;
+
+    @Column(name = "first_name")
+    @NonNull
+    private String firstName;
+
+    @Column(name = "last_name")
+    @NonNull
+    private String lastName;
+
+    @Column(name = "password")
+    @Transient
+    private String password;
+
+    @Column(name = "gender")
+    private String gender;
+
+    @Column(name = "is_active")
+    private boolean isActive;
+}
