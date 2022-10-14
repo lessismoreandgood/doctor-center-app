628dc33904d93e640d1fc9002ef1dc0e5fddf59e
nzharkynbek <nzharkynbek@one.kz>
2022-10-14 23:47:10 +0600

    feature(03). Added for appsprops file and fixed some stuff for db ( banner file as a bonus :) ).

--- src/main/java/com/example/doctorcenterapp/model/User.java
+++ src/main/java/com/example/doctorcenterapp/model/User.java
@@ -16,16 +16,13 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private String userId;
 
-    @Column(name = "username", nullable = false, unique = true)
-    @NonNull()
+    @Column(name = "username")
     private String email;
 
     @Column(name = "first_name")
-    @NonNull
     private String firstName;
 
     @Column(name = "last_name")
-    @NonNull
     private String lastName;
 
     @Column(name = "password")
